f63de074270d1ccb7608a533d42c5d64cd72bda4
苗高林 <2825873215@qq.com>
2021-12-19 03:00:44 +0800

    Add: Set the Maximum of block children freely

--- utils/notion.js
+++ utils/notion.js
@@ -1,14 +1,33 @@
-exports.getBlockChildren = async (notionClient, block_id) => {
+exports.getBlockChildren = async (notionClient, block_id, totalPage) => {
   try {
     if (!notionClient) {
       throw new Error(
         "notion client is not provided, for more details check out https://github.com/souvikinator/notion-to-md"
       );
     }
-    const response = await notionClient.blocks.children.list({
-      block_id,
-    });
-    return response.results;
+    let result=[];
+    let start_cursor;
+    let pageSize = 100;
+    for (i = 0; i < totalPage; i++) {
+      // contain start_cursor
+      const response = await notionClient.blocks.children.list({
+        start_cursor: start_cursor,
+        page_size: pageSize,
+        block_id: block_id
+      });
+      let current = response.results;
+      // delete start_cursor
+      if (start_cursor) {
+        current.shift();
+      }
+      result.push(...current);
+      if (current.length < 100 ) {
+        break;
+      }
+      pageSize = 101
+      start_cursor = current[current.length - 1].id;
+    }
+    return result;
   } catch (e) {
     console.log(e);
   }
