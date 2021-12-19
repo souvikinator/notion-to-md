0f7e58fe516c75c529550cb5d9c8980be23a9957
Souvik Kar Mahapatra <souvikat001@gmail.com>
2021-12-19 20:48:18 +0530

    updated definition file

--- main.d.ts
+++ main.d.ts
@@ -1,6 +1,18 @@
-export default class notion2md {
-    constructor(options: {notionClient: any});
+interface MdBlock {
+  parent: string;
+  children: MdBlock[];
+}
 
-    toMarkdownString(mdBlocks: Array<any>, nestingLevel?: number): string;
-    async pageToMarkdown(id: string): Array<any>
+declare module "notion-to-md" {
+  export default class Notion2md {
+    constructor({ notionClient }: { notionClient: any });
+    pageToMarkdown(page_id: string, totalPage: number = 1): Promise<MdBlock[]>;
+    toMarkdownString(mdBlocks: MdBlock[]): string;
+    blocksToMarkdown(
+      blocks: any,
+      mdBlocks: MdBlock[],
+      totalPage: number = 1
+    ): Promise<MdBlock[]>;
+    blockToMarkdown(block: any): string;
+  }
 }
