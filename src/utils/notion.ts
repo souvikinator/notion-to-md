62b3689bc2d6b632485dc1c56eb600fd52d95e19
Souvik Kar Mahapatra <souvikat001@gmail.com>
2022-01-29 07:47:19 +0530

    set totalPage default value to null

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -4,23 +4,26 @@ import { ListBlockChildrenResponse } from "@notionhq/client/build/src/api-endpoi
 export const getBlockChildren = async (
   notionClient: Client,
   block_id: string,
-  totalPage: number
+  totalPage: number | null
 ) => {
   try {
     let result = [];
     let pageCount = 0;
     let start_cursor = undefined;
-    
+
     do {
       const response = (await notionClient.blocks.children.list({
         start_cursor: start_cursor,
         block_id: block_id,
       })) as ListBlockChildrenResponse;
       result.push(...response.results);
-      
+
       start_cursor = response?.next_cursor;
       pageCount += 1;
-    } while(start_cursor != null && (totalPage == null || pageCount < totalPage))
+    } while (
+      start_cursor != null &&
+      (totalPage == null || pageCount < totalPage)
+    );
     return result;
   } catch (e) {
     console.log(e);
