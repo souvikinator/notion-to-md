1f4229e863176f1694abd80929cd566cdd0ef32d
souvik kar mahapatra <souvikat001@gmail.com>
2022-06-09 21:21:15 +0530

    added test for table()

--- src/utils/md.spec.ts
+++ src/utils/md.spec.ts
@@ -1,18 +1,37 @@
-import { callout } from './md';
+import { callout, table } from "./md";
 
-describe('Callout', () => {
-    test('parses callout without emoji', () => {
-        const text = 'Call out text content.';
+describe("Callout", () => {
+  test("parses callout without emoji", () => {
+    const text = "Call out text content.";
 
-        expect(callout(text)).toBe(`> ${text}`);
-    });
+    expect(callout(text)).toBe(`> ${text}`);
+  });
 
-    test('parses callout with emoji', () => {
-        const text = 'Call out text content.';
+  test("parses callout with emoji", () => {
+    const text = "Call out text content.";
 
-        expect(callout(text, {
-            type: 'emoji',
-            emoji: '😍'
-        })).toBe(`> 😍 ${text}`);
-    });
+    expect(
+      callout(text, {
+        type: "emoji",
+        emoji: "😍",
+      })
+    ).toBe(`> 😍 ${text}`);
+  });
+
+  test("parse simple table to markdown table", () => {
+    const mockTable = [
+      ["number", "char"],
+      ["1", "a"],
+      ["2", "b"],
+    ];
+
+    expect(table(mockTable)).toBe(
+      `
+| number | char |
+| ------ | ---- |
+| 1      | a    |
+| 2      | b    |
+    `.trim()
+    );
+  });
 });
