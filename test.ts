713c5b800cbcc350028a1cd4f1c6744f157a15c3
Souvik <souvikinator@pop-os.localdomain>
2023-04-03 01:37:37 +0530

    added test file

---
+++ test.ts
@@ -0,0 +1,21 @@
+import { Client } from "@notionhq/client";
+import { NotionToMarkdown } from "./build";
+import fs from "fs";
+
+const notion = new Client({
+  auth: "",
+});
+
+// passing notion client to the option
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+(async () => {
+  // b4f23bb4d29d44408837c3f67df8fd64
+  const mdblocks = await n2m.pageToMarkdown("b4f23bb4d29d44408837c3f67df8fd64");
+  const mdString = n2m.toMarkdownString(mdblocks);
+
+  //writing to file
+  fs.writeFile("test.md", mdString, (err) => {
+    console.log(err);
+  });
+})();
